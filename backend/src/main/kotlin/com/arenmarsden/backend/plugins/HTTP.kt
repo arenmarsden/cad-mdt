f0955755edf342aed31a0a58c5af3a0bed03007f
Aren Marsden <aren.marsden6@gmail.com>
2023-01-14 23:08:07 +0000

    fix build
    Signed-off-by: Aren Marsden <aren.marsden6@gmail.com>

--- backend/src/main/kotlin/com/arenmarsden/backend/plugins/HTTP.kt
+++ backend/src/main/kotlin/com/arenmarsden/backend/plugins/HTTP.kt
@@ -1,11 +1,7 @@
 package com.arenmarsden.backend.plugins
 
-import io.ktor.server.plugins.openapi.*
-import io.ktor.server.routing.*
 import io.ktor.server.application.*
 
 fun Application.configureHTTP() {
-    routing {
-        openAPI(path = "openapi")
-    }
+
 }
--- backend/src/main/kotlin/com/arenmarsden/backend/plugins/Routing.kt
+++ backend/src/main/kotlin/com/arenmarsden/backend/plugins/Routing.kt
@@ -1,14 +1,12 @@
 package com.arenmarsden.backend.plugins
 
-import arenmarden.com.storage.repository.impl.userRepository
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         get {
-            call.respondText(mapOf("users" to userRepository.getAllUsers()).toString())
+
         }
     }
 }
